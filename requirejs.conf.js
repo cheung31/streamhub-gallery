f9d0f983f6b807afddf10726368a4611a2b8bce5
Ryan Cheung <ryanc@livefyre.com>
2013-10-07 15:22:26 -0700

    Remove anima dep

--- requirejs.conf.js
+++ requirejs.conf.js
@@ -1,7 +1,6 @@
 requirejs.config({
   baseUrl: '/',
   paths: {
-    anima: 'lib/anima/anima',
     jquery: 'lib/jquery/jquery',
 	jasmine: 'lib/jasmine/lib/jasmine-core/jasmine',
     'jasmine-html': 'lib/jasmine/lib/jasmine-core/jasmine-html',
