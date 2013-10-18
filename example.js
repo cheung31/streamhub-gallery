08ed9380a78a9ab0cc6c99ee3a0bf4f3a76b11fe
Ryan Cheung <ryanc@livefyre.com>
2013-10-17 17:21:15 -0700

    Update app gallery example

--- example.js
+++ example.js
@@ -1,5 +1,8 @@
 define(function(require) {
     var Collection = require('streamhub-sdk/collection');
+    var DefaultAnimator = require('streamhub-gallery/animators/animator');
+    var CoverflowAnimator = require('streamhub-gallery/animators/coverflow-animator');
+    var CarouselAnimator = require('streamhub-gallery/animators/coverflow-animator');
     var View = require('streamhub-gallery');
 
     return function(el) {
@@ -9,7 +12,11 @@ define(function(require) {
             siteId: "303827",
             articleId: 'sh_col_51_1366914813'
         });
-        var view = new View({el: el});
+        var view = new View({
+            el: el,
+            aspectRatio: 4/3
+        });
+        view.switchAnimator(new CoverflowAnimator());
         
         collection.pipe(view);
   
